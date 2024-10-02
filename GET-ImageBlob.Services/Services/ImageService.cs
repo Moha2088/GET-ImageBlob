ecd81261a9e25df9ec0a41639b47dd86645f44ba
Moha2088 <113173091+Moha2088@users.noreply.github.com>
2024-10-02 10:06:57 +0200

    config

--- GET-ImageBlob.Services/Services/ImageService.cs
+++ GET-ImageBlob.Services/Services/ImageService.cs
@@ -3,6 +3,7 @@ using Azure.Identity;
 using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
 using GET_ImageBlob.Services.Services.Interfaces;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System.Text;
 
@@ -11,14 +12,17 @@ namespace GET_ImageBlob.Services.Services;
 public class ImageService : IImageService
 {
     private readonly BlobServiceClient _blobServiceClient;
-    private readonly string _storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=cloudstoragemo;AccountKey=opISwGATdy6zr8sCExJf2qKT84R/gcQHdFxu7yxSlXMO/GOixmvizSnzg/6RYmMcXZZDIOwA4aw8+AStIu9NBQ==;EndpointSuffix=core.windows.net";
-    private readonly string _blobContainer = "cloudstoragemocontainer";
+    private readonly string _storageConnectionString;
+    private readonly string _blobContainer;
     private readonly ILogger<ImageService> _logger;
     
     public ImageService(ILogger<ImageService> logger) 
     {
-        _blobServiceClient = new BlobServiceClient(_storageConnectionString);
         _logger = logger;
+        IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+        _storageConnectionString = config.GetSection("AzureCredentials:StorageConnectionString").Value!;
+        _blobContainer = config.GetSection("AzureCredentials:BlobContainerName").Value!;
+        _blobServiceClient = new BlobServiceClient(_storageConnectionString);
     }
 
     public async Task UploadImageBlob(string localFilePath, CancellationToken cancellationToken)
